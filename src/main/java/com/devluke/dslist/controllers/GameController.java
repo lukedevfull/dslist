7d5c4eeb56dbadec28f6c1f514ed508c815b5291
Lukedevfull <contato.prof2@gmail.com>
2025-05-31 14:17:41 -0300

    get game by id and configure GameDTO

--- src/main/java/com/devluke/dslist/controllers/GameController.java
+++ src/main/java/com/devluke/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devluke.dslist.controllers;
 
+import com.devluke.dslist.dto.GameDTO;
 import com.devluke.dslist.dto.GameMinimalDTO;
 import com.devluke.dslist.entities.Game;
 import com.devluke.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,13 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{gameId}")
+    public GameDTO findById(@PathVariable Long gameId){
+        return gameService.findById(gameId);
+    }
+
     @GetMapping
     public List<GameMinimalDTO> findAll(){
-        var result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 }
--- src/main/java/com/devluke/dslist/services/GameService.java
+++ src/main/java/com/devluke/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devluke.dslist.services;
 
+import com.devluke.dslist.dto.GameDTO;
 import com.devluke.dslist.dto.GameMinimalDTO;
 import com.devluke.dslist.entities.Game;
 import com.devluke.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId){
+        Game result = gameRepository.findById(gameId).get();
+        return new GameDTO(result);
+    }
+    
+    @Transactional(readOnly = true)
     public List<GameMinimalDTO> findAll(){
 
         var result = gameRepository.findAll();
