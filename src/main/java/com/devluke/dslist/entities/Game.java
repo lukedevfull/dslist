cac6513baf6f9fe388c2cdc2605dcf4eac6fba1a
Lukedevfull <contato.prof2@gmail.com>
2025-05-31 00:04:23 -0300

    Update a Game, and Created GameList, BelongingPrimaryKey and Belonging, finished the TB SCHEMA on Jakarta

--- src/main/java/com/devluke/dslist/entities/Game.java
+++ src/main/java/com/devluke/dslist/entities/Game.java
@@ -43,7 +43,8 @@ public class Game {
         this.longDescription = longDescription;
     }
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
@@ -52,66 +53,82 @@ public class Game {
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public Integer getYear() {
+
         return year;
     }
 
     public void setYear(Integer year) {
+
         this.year = year;
     }
 
     public String getGenre() {
+
         return genre;
     }
 
     public void setGenre(String genre) {
+
         this.genre = genre;
     }
 
     public String getPlatforms() {
+
         return platforms;
     }
 
     public void setPlatform(String platforms) {
+
         this.platforms = platforms;
     }
 
     public Double getScore() {
+
         return score;
     }
 
     public void setScore(Double score) {
+
         this.score = score;
     }
 
     public String getImgUrl() {
+
         return imgUrl;
     }
 
     public void setImgUrl(String imgUrl) {
+
         this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
+
         return shortDescription;
     }
 
     public void setShortDescription(String shortDescription) {
+
         this.shortDescription = shortDescription;
     }
 
     public String getLongDescription() {
+
         return longDescription;
     }
 
     public void setLongDescription(String longDescription) {
+
         this.longDescription = longDescription;
     }
 
